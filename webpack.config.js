9c115f69a583a306e51a2856b70272170057255d
Josef <josefwijaya@outlook.com>
2018-07-10 16:15:56 +0800

    update webpack config - update file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'tenderkeys.min.js',
     library: 'TenderKeys'
   }
 };
