c874f3f38c284e51a83b748bc1aa581677b43d15
Josef <josefwijaya@outlook.com>
2018-07-20 13:19:44 +0800

    update crypto - added getPubKeyByPrivKey

--- test/util.test.js
+++ test/util.test.js
@@ -76,6 +76,7 @@ describe('GallacticKeys - utils - crypto', function () {
   it('should have "decrypt" function', () => expect(crypto.decrypt).to.be.a('function'));
   it('should have "getAddressByPubKey" function', () => expect(crypto.getAddressByPubKey).to.be.a('function'));
   it('should have "getAddressByPrivKey" function', () => expect(crypto.getAddressByPrivKey).to.be.a('function'));
+  it('should have "getPubKeyByPrivKey" function', () => expect(crypto.getPubKeyByPrivKey).to.be.a('function'));
   it('should have "encodeAddress" function', () => expect(crypto.encodeAddress).to.be.a('function'));
   it('should have "isCipherAvailable" function', () => expect(crypto.isCipherAvailable).to.be.a('function'));
   it('should have "makeKeyPairFromSeed" function', () => expect(crypto.makeKeyPairFromSeed).to.be.a('function'));
@@ -130,6 +131,12 @@ describe('GallacticKeys - utils - crypto', function () {
 
   it('"getAddressByPubKey" should return ')
   it('"getAddressByPrivKey" should return ')
+  it('"getPubKeyByPrivKey" should return public key given private key', function () {
+    let privKey = '8EAB2233E0DCE2F1337BD491B2EB04CA6C8334B60C1FB0D1A9B6C80CABF1765D774D6DC700FB0BDE7924BA1CA27EDAEC9F51939824BC20300FAA468285AEDE08';
+    let result = crypto.getPubKeyByPrivKey(privKey);
+    expect(result).to.be.a('string');
+    expect(result.length).to.equal(64);
+  });
 
   it('"encodeAddress", should return the encoded address based on given address and option', function (done) {
     const test = {
